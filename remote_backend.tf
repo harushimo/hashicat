f87b1f14d4abe2d50907122371d8ad483499dacb
Ivan Jonota <ivan.jonota@gmail.com>
2019-06-03 21:07:56 +0000

    update files

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "hashicorp-workshop"
+    organization = "ijonota-sandbox"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "ijonota-ws"
     }
   }
 }
